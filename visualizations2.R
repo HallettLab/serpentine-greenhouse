386f47f3a68c0e64a91f7604d0049260da703f69
Eliza Hernandez <elizahrndez@gmail.com>
2020-11-18 13:54:12 -0800

    somewhat final graphs for raw data of seed production

--- visualizations2.R
+++ visualizations2.R
@@ -16,6 +16,8 @@ ggplot(seed_dat_nodens, aes(trt_N,mean_out_in, group = interaction(individual, t
 ggplot(seed_dat_nodens, aes(trt_N,mean_out_in)) + geom_point(aes(color = background_comp)) +
   facet_wrap(~trt_water)
 
+seed_dat <- read.csv(paste(datpath, "/clean_seed_dat.csv", sep = ""))
+
 seed_dat <- seed_dat %>%
   group_by(seed_density,individual,background_comp,trt_water,trt_N) %>%
   summarize(mean_out_in = mean(out_in, na.rm = T),se_out_in = calcSE(out_in))
@@ -23,22 +25,21 @@ seed_dat <- seed_dat %>%
 seed_dat$trt_water <- factor(seed_dat$trt_water , levels = c("lo","hi"))
 seed_dat$trt_N <- factor(seed_dat$trt_N , levels = c("lo","int","hi"))
 
-
-hi_dens <- ggplot(subset(seed_dat, !seed_density %in% c("none","lo")), aes(trt_N,mean_out_in,group = interaction(background_comp, seed_density))) + 
+ggplot(subset(seed_dat, !seed_density %in% c("none","lo") & individual %in% "LAPL"), aes(trt_N,mean_out_in,group = interaction(background_comp, seed_density))) + 
   geom_point(aes(color = background_comp, shape = seed_density),size=2.5) + geom_line(aes(color = background_comp)) +
-  facet_grid(individual~trt_water,scale = "free") + ylab("Per capita seed production") 
+  facet_grid(individual~trt_water,scale = "free") + ylab("Per capita seed production") +
+  theme_bw() + ylim(0,20)
 
-ggsave("hi_dens.png")
 
-lo_dens <- ggplot(subset(seed_dat, !seed_density %in% c("none","hi")), aes(trt_N,mean_out_in,group = interaction(background_comp, seed_density))) + 
+ggplot(subset(seed_dat, !seed_density %in% c("none","hi") & individual %in% "PLER" & !background_comp %in% "PLER"), aes(trt_N,mean_out_in,group = interaction(background_comp, seed_density))) + 
   geom_point(aes(color = background_comp, shape = seed_density),size=2.5) + geom_line(aes(color = background_comp)) +
-  facet_grid(individual~trt_water,scale = "free") + ylab("Per capita seed production") 
+  facet_grid(individual~trt_water,scale = "free") + ylab("Per capita seed production") + 
+  theme_bw() + ylim(0,20)
 
-ggsave("lo_dens.png")
 
-none_dens <- ggplot(subset(seed_dat, seed_density %in% "none"), aes(trt_N,mean_out_in,group = interaction(background_comp, seed_density))) + 
+ggplot(subset(seed_dat, seed_density %in% "none" & individual %in% "LAPL"), aes(trt_N,mean_out_in,group = interaction(background_comp, seed_density))) + 
   geom_point(aes(color = background_comp, shape = seed_density),size=2.5) + geom_line(aes(color = background_comp)) +
-  facet_grid(individual~trt_water,scale = "free") + ylab("Per capita seed production") 
+  facet_grid(individual~trt_water,scale = "free") + ylab("Per capita seed production") + theme_bw()
 
 ggsave("none_dens.png")
 
