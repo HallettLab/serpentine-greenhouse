54cb04a2f669db363d730e273e282cc6fee96396
Eliza Hernandez <elizahrndez@gmail.com>
2021-02-08 14:10:02 -0800

    Finished cleaning up data for model with both 0 and 1 for seeds in and one csv file for some bromus samples adjusted and another csv file for all bromus samples adjusted

--- Data cleaning/tidying_data.R
+++ Data cleaning/tidying_data.R
@@ -4,7 +4,6 @@
 
 library(tidyverse)
 
-setwd("~/Repositories/Serpentine-greenhouse/Data cleaning")
 source("fitting_seed_biomass.R")
 
 ## Read in other data
@@ -93,56 +92,136 @@ clean_dat2 <- full_join(join1a,join2) %>%
 #write.csv (clean_dat1, "clean_dat1.csv")
 #write.csv(clean_dat1, "clean_dat2.csv")
 
-## Clean up for models
+## Clean up for models with seeds in as zero
 #BRHO phytometer and background seed/biomass production
 brho_back_adj1 <- brho_back_adj1 %>%
-  mutate(PLER_seeds_in = 0, LAPL_seeds_in = 0, FEMI_seeds_in = 0)
+  mutate(PLER_seeds_in = 0, LAPL_seeds_in = 0, FEMI_seeds_in = 0) %>%
+  rename(BRHO_seeds_in = seeds_in)
 
 brho_phyt_adj1 <- brho_phyt_adj1 %>%
-  inner_join(dens1)
+  inner_join(dens1) %>%
+  select(-seeds_in)
 
 brho_back_adj2 <- brho_back_adj2 %>%
-  mutate(PLER_seeds_in = 0, LAPL_seeds_in = 0, FEMI_seeds_in = 0)
+  mutate(PLER_seeds_in = 0, LAPL_seeds_in = 0, FEMI_seeds_in = 0) %>%
+  rename(BRHO_seeds_in = seeds_in)
 
 brho_phyt_adj2 <- brho_phyt_adj2 %>%
-  inner_join(dens1)
+  inner_join(dens1) %>%
+  select(-seeds_in)
 
 # FEMI phytometer and background seed/biomass production
 femi_back_adj <- femi_back_adj %>%
-  mutate(PLER_seeds_in = 0, LAPL_seeds_in = 0, FEMI_seeds_in = 0)
+  mutate(PLER_seeds_in = 0, LAPL_seeds_in = 0, BRHO_seeds_in = 0) %>%
+  rename(FEMI_seeds_in = seeds_in)
 
-femi_phyt <- phyt %>%
-  inner_join(dens1)
+femi_phyt <- femi_phyt %>%
+  inner_join(dens1) %>%
+  select(-seeds_in)
 
 # PLER phytometer and background seed/biomass production
-pler_back <- back %>%
-  mutate(PLER_seeds_in = 0, LAPL_seeds_in = 0, FEMI_seeds_in = 0)
+pler_back <- pler_back %>%
+  mutate(BRHO_seeds_in = 0, LAPL_seeds_in = 0, FEMI_seeds_in = 0) %>%
+  rename(PLER_seeds_in = seeds_in)
 
-pler_phyt <- phyt %>%
-  inner_join(dens1)
+pler_phyt <- pler_phyt %>%
+  inner_join(dens1) %>%
+  select(-seeds_in)
 
 # LAPL phytometer and and background seed/biomass production
-lapl_back <- back %>%
-  mutate(PLER_seeds_in = 0, LAPL_seeds_in = 0, FEMI_seeds_in = 0)
+lapl_back <- lapl_back %>%
+  mutate(PLER_seeds_in = 0, BRHO_seeds_in = 0, FEMI_seeds_in = 0) %>%
+  rename(LAPL_seeds_in = seeds_in)
 
-lapl_phyt <- phyt %>%
-  inner_join(dens)
+lapl_phyt <- lapl_phyt %>%
+  inner_join(dens1) %>%
+  select(-seeds_in)
 
 # Join all species
-brho_dat <- full_join(brho_back,brho_phyt)
-brho_dat2 <- full_join(brho_back2,brho_phyt2)
-femi_dat <- full_join(femi_back,femi_phyt)
+brho_dat <- full_join(brho_back_adj1,brho_phyt_adj1)
+brho_dat2 <- full_join(brho_back_adj2,brho_phyt_adj2)
+femi_dat <- full_join(femi_back_adj,femi_phyt)
 pler_dat <- full_join(pler_back,pler_phyt)
 lapl_dat <- full_join(lapl_back,lapl_phyt)
 join1 <- full_join(brho_dat,femi_dat)
 join1a <- full_join(brho_dat2,femi_dat)
 join2 <- full_join(pler_dat,lapl_dat)
-model_dat1 <- full_join(join1,join2)
-model_dat2 <- full_join(join1a,join2)
+model_dat1 <- full_join(join1,join2) %>%
+  select(-biomass_g)
+model_dat2 <- full_join(join1a,join2) %>%
+  select(-biomass_g)
 
-#Write file
+# Write file
+# Some bromus samples adjusted
+ #write.csv(model_dat1, "model_dat_1.csv")
+# All bromus samples adjusted
+ #write.csv(model_dat2, "model_dat_0.csv")
+
+## Clean up for models with seeds in as 1
+#BRHO phytometer and background seed/biomass production
+#brho_back_adj1 <- brho_back_adj1 %>%
+#  mutate(PLER_seeds_in = 1, LAPL_seeds_in = 1, FEMI_seeds_in = 1) %>%
+#  rename(BRHO_seeds_in = seeds_in)
+
+#brho_phyt_adj1 <- brho_phyt_adj1 %>%
+#  inner_join(dens2) %>%
+#  select(-seeds_in)
+
+#brho_back_adj2 <- brho_back_adj2 %>%
+#  mutate(PLER_seeds_in = 1, LAPL_seeds_in = 1, FEMI_seeds_in = 1) %>%
+#  rename(BRHO_seeds_in = seeds_in)
+
+#brho_phyt_adj2 <- brho_phyt_adj2 %>%
+#  inner_join(dens2) %>%
+#  select(-seeds_in)
+
+# FEMI phytometer and background seed/biomass production
+#femi_back_adj <- femi_back_adj %>%
+#  mutate(PLER_seeds_in = 1, LAPL_seeds_in = 1, BRHO_seeds_in = 1) %>%
+#  rename(FEMI_seeds_in = seeds_in)
+
+#femi_phyt <- femi_phyt %>%
+#  inner_join(dens2) %>%
+#  select(-seeds_in)
+
+# PLER phytometer and background seed/biomass production
+#pler_back <- pler_back %>%
+#  mutate(BRHO_seeds_in = 1, LAPL_seeds_in = 1, FEMI_seeds_in = 1) %>%
+#  rename(PLER_seeds_in = seeds_in)
+
+#pler_phyt <- pler_phyt %>%
+#  inner_join(dens2) %>%
+#  select(-seeds_in)
+
+# LAPL phytometer and and background seed/biomass production
+#lapl_back <- lapl_back %>%
+#  mutate(PLER_seeds_in = 1, BRHO_seeds_in = 1, FEMI_seeds_in = 1) %>%
+#  rename(LAPL_seeds_in = seeds_in)
+
+#lapl_phyt <- lapl_phyt %>%
+#  inner_join(dens2) %>%
+#  select(-seeds_in)
+
+# Join all species
+#brho_dat <- full_join(brho_back_adj1,brho_phyt_adj1)
+#brho_dat2 <- full_join(brho_back_adj2,brho_phyt_adj2)
+#femi_dat <- full_join(femi_back_adj,femi_phyt)
+#pler_dat <- full_join(pler_back,pler_phyt)
+#lapl_dat <- full_join(lapl_back,lapl_phyt)
+#join1 <- full_join(brho_dat,femi_dat)
+#join1a <- full_join(brho_dat2,femi_dat)
+#join2 <- full_join(pler_dat,lapl_dat)
+#model_dat1 <- full_join(join1,join2) %>%
+#  select(-biomass_g)
+#model_dat2 <- full_join(join1a,join2) %>%
+#  select(-biomass_g)
+
+# Write file
+# Some bromus samples adjusted
 #write.csv(model_dat1, "model_dat_1.csv")
+# All bromus samples adjusted
 #write.csv(model_dat2, "model_dat_0.csv")
+
 
 
 
