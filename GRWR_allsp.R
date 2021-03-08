02a84e778686e4d927bc415bbb9b4e200a9c4e0f
Eliza Hernandez <elizahrndez@gmail.com>
2021-03-08 10:53:49 -0800

    Updated to include graphs of abundances at equlibirum and when rare

--- GRWR_allsp.R
+++ GRWR_allsp.R
@@ -140,6 +140,7 @@ consistent.grwr.out <- consistent.out %>%
 
 ## GRWR graph
 consistent.grwr.out$N <- factor(consistent.grwr.out$N, levels = c("lo","int","hi"))
+consistent.grwr.out$water <- factor(consistent.grwr.out$water, levels = c("lo","hi"))
 
 ggplot(consistent.grwr.out, aes(x=N, y=grwrChesson, fill = water)) + 
   geom_bar(stat="identity", position = "dodge") + 
@@ -161,10 +162,10 @@ consistent.out <- rbind(hi_hi_grwr, hi_int_grwr, hi_lo_grwr, lo_hi_grwr, lo_int_
 
 consistent.out$N <- factor(consistent.out$N, levels = c("lo","int","hi"))
 consistent.out$water <- factor(consistent.out$water, levels = c("lo","hi"))
-consistent.out$invader <- factor(consistent.out$invader, levels = c("Plantago","Bromus"))
+consistent.out$invader <- factor(consistent.out$invader, levels = c("Bromus","Plantago"))
 consistent.out$species <- factor(consistent.out$species, levels = c("Np","Nb"))
 cond.labs <- c("*Plantago* starting condition","*Bromus* starting condition")
-names(cond.labs) <- c("Plantago","Bromus")
+names(cond.labs) <- c("Bromus","Plantago")
 N.labs<- c("Low N","Intermediate N","High N")
 names(N.labs) <- c("lo","int","hi")
 
@@ -176,8 +177,7 @@ ggplot(consistent.out,aes(time,abundance,group=interaction(species,water))) +
   scale_color_manual(name = "Water treatments",labels=c("Dry","Wet"),
                        values=c("grey80","grey40")) +
   xlab("Year") + ylab(expression(Count~(individuals~per~100~cm^{"2"}))) +
-  theme(legend.text = element_markdown(),strip.text.y = element_markdown())
-
+  theme(legend.text = element_markdown(),strip.text.y = element_markdown()) 
 
 ###################################
 ####### Bromus and Festuca ########
@@ -298,6 +298,7 @@ consistent.grwr.out <- consistent.out %>%
 
 ## GRWR graph
 consistent.grwr.out$N <- factor(consistent.grwr.out$N, levels = c("lo","int","hi"))
+consistent.grwr.out$water <- factor(consistent.grwr.out$water, levels = c("lo","hi"))
 
 ggplot(consistent.grwr.out, aes(x=N, y=grwrChesson, fill = water)) + 
   geom_bar(stat="identity", position = "dodge") + 
@@ -319,10 +320,10 @@ consistent.out <- rbind(hi_hi_grwr, hi_int_grwr, hi_lo_grwr, lo_hi_grwr, lo_int_
 
 consistent.out$N <- factor(consistent.out$N, levels = c("lo","int","hi"))
 consistent.out$water <- factor(consistent.out$water, levels = c("lo","hi"))
-consistent.out$invader <- factor(consistent.out$invader, levels = c("Festuca","Bromus"))
+consistent.out$invader <- factor(consistent.out$invader, levels = c("Bromus","Festuca"))
 consistent.out$species <- factor(consistent.out$species, levels = c("Nf","Nb"))
 cond.labs <- c("*Festuca* starting condition","*Bromus* starting condition")
-names(cond.labs) <- c("Festuca","Bromus")
+names(cond.labs) <- c("Bromus","Festuca")
 N.labs<- c("Low N","Intermediate N","High N")
 names(N.labs) <- c("lo","int","hi")
 
@@ -457,6 +458,7 @@ consistent.grwr.out <- consistent.out %>%
 
 ## GRWR graph
 consistent.grwr.out$N <- factor(consistent.grwr.out$N, levels = c("lo","int","hi"))
+consistent.grwr.out$water <- factor(consistent.grwr.out$water, levels = c("lo","hi"))
 
 ggplot(consistent.grwr.out, aes(x=N, y=grwrChesson, fill = water)) + 
   geom_bar(stat="identity", position = "dodge") + 
@@ -478,10 +480,10 @@ consistent.out <- rbind(hi_hi_grwr, hi_int_grwr, hi_lo_grwr, lo_hi_grwr, lo_int_
 
 consistent.out$N <- factor(consistent.out$N, levels = c("lo","int","hi"))
 consistent.out$water <- factor(consistent.out$water, levels = c("lo","hi"))
-consistent.out$invader <- factor(consistent.out$invader, levels = c("Layia","Bromus"))
+consistent.out$invader <- factor(consistent.out$invader, levels = c("Bromus","Layia"))
 consistent.out$species <- factor(consistent.out$species, levels = c("Nl","Nb"))
 cond.labs <- c("*Layia* starting condition","*Bromus* starting condition")
-names(cond.labs) <- c("Layia","Bromus")
+names(cond.labs) <- c("Bromus","Layia")
 N.labs<- c("Low N","Intermediate N","High N")
 names(N.labs) <- c("lo","int","hi")
 
@@ -617,6 +619,8 @@ consistent.grwr.out <- consistent.out %>%
 
 ## GRWR graph
 consistent.grwr.out$N <- factor(consistent.grwr.out$N, levels = c("lo","int","hi"))
+consistent.grwr.out$water <- factor(consistent.grwr.out$water, levels = c("lo","hi"))
+
 
 ggplot(consistent.grwr.out, aes(x=N, y=grwrChesson, fill = water)) + 
   geom_bar(stat="identity", position = "dodge") + 
@@ -638,10 +642,10 @@ consistent.out <- rbind(hi_hi_grwr, hi_int_grwr, hi_lo_grwr, lo_hi_grwr, lo_int_
 
 consistent.out$N <- factor(consistent.out$N, levels = c("lo","int","hi"))
 consistent.out$water <- factor(consistent.out$water, levels = c("lo","hi"))
-consistent.out$invader <- factor(consistent.out$invader, levels = c("Plantago","Festuca"))
+consistent.out$invader <- factor(consistent.out$invader, levels = c("Festuca","Plantago"))
 consistent.out$species <- factor(consistent.out$species, levels = c("Np","Nf"))
 cond.labs <- c("*Plantago* starting condition","*Festuca* starting condition")
-names(cond.labs) <- c("Plantago","Festuca")
+names(cond.labs) <- c("Festuca","Plantago")
 N.labs<- c("Low N","Intermediate N","High N")
 names(N.labs) <- c("lo","int","hi")
 
@@ -776,6 +780,7 @@ consistent.grwr.out <- consistent.out %>%
 
 ## GRWR graph
 consistent.grwr.out$N <- factor(consistent.grwr.out$N, levels = c("lo","int","hi"))
+consistent.grwr.out$water <- factor(consistent.grwr.out$water, levels = c("lo","hi"))
 
 ggplot(consistent.grwr.out, aes(x=N, y=grwrChesson, fill = water)) + 
   geom_bar(stat="identity", position = "dodge") + 
@@ -797,10 +802,10 @@ consistent.out <- rbind(hi_hi_grwr, hi_int_grwr, hi_lo_grwr, lo_hi_grwr, lo_int_
 
 consistent.out$N <- factor(consistent.out$N, levels = c("lo","int","hi"))
 consistent.out$water <- factor(consistent.out$water, levels = c("lo","hi"))
-consistent.out$invader <- factor(consistent.out$invader, levels = c("Plantago","Layia"))
+consistent.out$invader <- factor(consistent.out$invader, levels = c("Layia","Plantago"))
 consistent.out$species <- factor(consistent.out$species, levels = c("Np","Nl"))
 cond.labs <- c("*Plantago* starting condition","*Layia* starting condition")
-names(cond.labs) <- c("Plantago","Layia")
+names(cond.labs) <- c("Layia","Plantago")
 N.labs<- c("Low N","Intermediate N","High N")
 names(N.labs) <- c("lo","int","hi")
 
@@ -935,6 +940,7 @@ consistent.grwr.out <- consistent.out %>%
 
 ## GRWR graph
 consistent.grwr.out$N <- factor(consistent.grwr.out$N, levels = c("lo","int","hi"))
+consistent.grwr.out$water <- factor(consistent.grwr.out$water, levels = c("lo","hi"))
 
 ggplot(consistent.grwr.out, aes(x=N, y=grwrChesson, fill = water)) + 
   geom_bar(stat="identity", position = "dodge") + 
@@ -956,10 +962,10 @@ consistent.out <- rbind(hi_hi_grwr, hi_int_grwr, hi_lo_grwr, lo_hi_grwr, lo_int_
 
 consistent.out$N <- factor(consistent.out$N, levels = c("lo","int","hi"))
 consistent.out$water <- factor(consistent.out$water, levels = c("lo","hi"))
-consistent.out$invader <- factor(consistent.out$invader, levels = c("Festuca","Layia"))
+consistent.out$invader <- factor(consistent.out$invader, levels = c("Layia","Festuca"))
 consistent.out$species <- factor(consistent.out$species, levels = c("Nf","Nl"))
 cond.labs <- c("*Festuca* starting condition","*Layia* starting condition")
-names(cond.labs) <- c("Festuca","Layia")
+names(cond.labs) <- c("Layia","Festuca")
 N.labs<- c("Low N","Intermediate N","High N")
 names(N.labs) <- c("lo","int","hi")
 
