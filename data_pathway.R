84342a72c3b3278c397545fbaee9adf22cebed49
Eliza Hernandez <elizahrndez@gmail.com>
2021-12-25 09:12:58 -0800

    Updated data pathway after cleaning up Dropbox folder

--- data_pathway.R
+++ data_pathway.R
@@ -2,11 +2,8 @@
 ######Data pathways######
 #########################
 
-##pc user = eliza
-datpath <- "C:/Users/eliza/OneDrive - University Of Oregon/Serpentine/Eliza masters project/Greenhouse Experiment/Data/CleanData/"
-
-##pc user = eliza (different folder)
-datpath <- "C:/Users/eliza/OneDrive - University Of Oregon/Serpentine/Eliza masters project/Greenhouse Experiment/Data/CleanData/Cleaning/Raw data/"
+##pc user = eliza 
+datpath <- "C:/Users/eliza/Dropbox (University Of Oregon)/Serpentine-Eliza/Greenhouse ms/Data/"
 
 ##mac user = lauren
-datpath <- "~/University Of Oregon/Eliza Hernandez - Serpentine/Eliza masters project/Greenhouse Experiment/Data/CleanData"
+datpath <- "~/Dropbox (University Of Oregon)/Serpentine-Eliza/Greenhouse ms/Data"
