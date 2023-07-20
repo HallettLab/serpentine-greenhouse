ff511ca570117406169f16c91ca347841943792c
Eliza Hernandez <ehernan2@uoregon.edu>
2023-07-20 09:52:10 -0700

    cleaned up script to produce graph all the way through

--- Jasper-Ridge-sp-cover.R
+++ Jasper-Ridge-sp-cover.R
@@ -2,6 +2,8 @@ library(tidyverse)
 library(ggtext)
 library(gridExtra)
 library(grid)
+library(cowplot)
+library(ggplotify)
 
 theme_set(theme_bw())
 theme_update( panel.grid.major=element_blank(), panel.grid.minor=element_blank(),
@@ -32,6 +34,29 @@ cov <- cover %>%
   mutate_at(4,as.character) %>%
   mutate_at(6,as.numeric) %>%
   select(-8)
+
+# create a key to aggregate 0.5 m x 0.5 m subplots into 1 m x 1 m plots
+top_block1 <- cbind(c(1,2,7,8), rep("block1_top", 4))
+top_block2 <- cbind(c(13, 14,19, 20), rep("block2_top", 4))
+
+middle_block1 <- cbind(c(3,4,9,10), rep("block1_middle", 4))
+middle_block2 <- cbind(c(15, 16, 21, 22), rep("block2_middle", 4))
+
+bottom_block1 <- cbind(c(5, 6, 11, 12), rep("block1_bottom", 4))
+bottom_block2 <- cbind(c(17,18, 23, 24 ), rep("block2_bottom", 4))
+
+key <-as.data.frame(rbind(top_block1, top_block2, 
+                          middle_block1, middle_block2, 
+                          bottom_block1, bottom_block2))
+names(key)=c("subplot", "plot")
+
+key <- key %>%
+  tbl_df() %>%
+  mutate(subplot=as.numeric(as.character(subplot)), 
+         plot=as.character(plot))
+
+rm(top_block1, top_block2, middle_block1, middle_block2, bottom_block1, bottom_block2)
+
 
 # merge JRcover and key; aggregate at the 1 m x 1 m plot scale
 JRm2cover <- merge(cov, key) %>%
@@ -69,7 +94,7 @@ jrdatCI <- left_join(jrdat,CI)
 jr <- ggplot(jrdatCI,aes(year,mean_cov,color=species)) + 
   geom_line(size=.8) + 
   geom_ribbon(aes(x= year, ymin=lower,ymax=upper, fill=species), alpha = .2) +
-  ylab("Cover (%)") +
+  ylab("Percent cover") +
   scale_color_manual(values=c("#D55E00","#0072B2","#009E73"),name = "Species", 
                      labels = c("*Bromus*", "*Layia*", "*Plantago*"))+
   scale_fill_manual(name = "Species",labels = c("*Bromus*","*Layia*","*Plantago*"),
@@ -79,6 +104,33 @@ jr <- ggplot(jrdatCI,aes(year,mean_cov,color=species)) +
   theme(legend.position = "none") +
   theme(axis.text.x = element_blank(),axis.title.x = element_blank(),axis.ticks.x = element_blank()) +
   theme(plot.margin = unit(c(.5,.8,.5,.5), "cm"))#, legend.position = "top")
+
+
+leg <- ggplot(jrdatCI,aes(year,mean_cov,color=species)) + 
+  geom_line(size=.8) + 
+  geom_ribbon(aes(x= year, ymin=lower,ymax=upper, fill=species), alpha = .2) +
+  ylab("Percent cover") +
+  scale_color_manual(values=c("#D55E00","#0072B2","#009E73"),name = "Species", 
+                     labels = c("*Bromus*", "*Layia*", "*Plantago*"))+
+  scale_fill_manual(name = "Species",labels = c("*Bromus*","*Layia*","*Plantago*"),
+                    values=c("#D55E00","#0072B2","#009E73")) +
+  scale_x_continuous(expand = c(0.04, 0.04)) +
+  xlab("Year") +
+  theme(legend.position = "top") +
+  theme(axis.text.x = element_blank(),axis.title.x = element_blank(),axis.ticks.x = element_blank()) +
+  theme(plot.margin = unit(c(.5,.8,.5,.5), "cm")) +
+  theme(legend.text = ggtext::element_markdown())
+  
+
+g_legend <- function(a.gplot){ 
+  tmp <- ggplot_gtable(ggplot_build(a.gplot)) 
+  leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box") 
+  legend <- tmp$grobs[[leg]] 
+  legend
+} 
+
+legend <- as.ggplot(g_legend(leg))
+
 
 
 #rainfall
@@ -112,13 +164,13 @@ p2 <- ggplot(ppt, aes(x=year)) +
   scale_y_continuous(breaks = seq(0,1600,by=565))
 
 gjr <- ggplotGrob(jr)
-gp <- ggplotGrob(p)
+gp <- ggplotGrob(p2)
 maxWidth = grid::unit.pmax(gjr$widths[2:5], gp$widths[2:5])
 gjr$widths[2:5] <- as.list(maxWidth)
 gp$widths[2:5] <- as.list(maxWidth)
 grid.arrange(gjr,gp,ncol=1,left = textGrob(c("a)","b)"), x =c(2.7,2.7), 
                                     y = c(.92,.51), gp = gpar(fontface = "bold", fontsize = 16)))
 
-plot_grid(legend,jr,p2,ncol=1,align="v",rel_heights = c(.05,.6,.15),labels = c("","a)","b)"),hjust = -3)
+plot_grid(legend,jr,p2,ncol=1,align="v",rel_heights = c(.05,.6,.15),labels = c("","a)","b)"),hjust = -3,vjust=0)
 
 
