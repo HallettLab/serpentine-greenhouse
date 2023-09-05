8cc819f85904ff66c5e9dfb3ae4a2fc602117534
Eliza Hernández <elizahrndez@gmail.com>
2023-09-05 15:28:00 -0700

    run models without FEMI and add g fractions for all species in BH equation

---
+++ Stand models w-out FEMI/BRHO_stan_model.R
@@ -0,0 +1,219 @@
+## Loads rstan and sets the number of useable cores based on your computer
+library(tidyverse)
+library(rstan)
+options(mc.cores = parallel::detectCores())
+rstan_options(auto_write = TRUE)
+
+## Read in data
+data <- read.csv(paste(datpath, "model_dat2_noFEMI.csv", sep = "")) %>%
+  select(-X)
+
+## Subset data for competitor and treatment of interest
+dat <- subset(data, species == "BRHO")
+dat <- subset(dat, waterN_treatment == "hi_hi")
+
+## Create model variables for our data
+### Set Fecundity as the seeds out from our focal species
+Fecundity <- as.integer(round(dat$seeds_out))
+
+### Set population context for each species as seeds in
+pler <- as.integer(dat$PLER_seeds_in)
+brho <- as.integer(dat$BRHO_seeds_in)
+lapl <- as.integer(dat$LAPL_seeds_in)
+
+### Number of observations 
+N <- as.integer(length(Fecundity))
+P <- as.integer(length(unique(dat$block)))
+
+### Set intra-specific species
+intra <- brho
+
+Plot <- dat$block
+
+bg <- 0.98
+pg <- 0.92
+lg <- 0.32
+
+######################################################
+
+# high high initials
+initials <- list(lambda=113, alpha_pler=0.05, alpha_brho=0.05, alpha_lapl=0.05,
+                 epsilon=rep(1,P), sigma = 10)
+initials1<- list
+
+brho_hi_hi <- stan(file = "brho_bevertonholt_model.stan", 
+                                 data = c("N", "Fecundity", "intra", "pler", "brho", "lapl", "P", "Plot","bg"),
+                                 iter = 2000, chains = 3, thin = 3, control = list(adapt_delta = 0.99999999999999, max_treedepth =50),
+                                 init = initials1)
+
+# high high initials
+initials <- list(lambda=235.2, alpha_pler=0.22, alpha_brho=1.09, alpha_lapl=0.24,
+                 epsilon=rep(1,P), sigma = 311.5)
+initials1<- list(initials, initials, initials)
+
+## Fit model
+### Specify the stan model, the data to send it by name, iterations, chains (based on number of cores), 
+### thinning constant (2 or 3 is usually fine), 
+# NOTE: NUMBER OF DIVERGENT TRANSITIONS IS 4; ALL RHAT VALUES ARE 1.00
+no_dist_seeds_brho_hi_hi <- stan(file = "BRHO_Constrained_rplot_four_species_BH_model.stan", 
+                                 data = c("N", "Fecundity", "intra", "pler", "brho", "lapl", "femi", "P", "Plot","bg"),
+                                 iter = 12000, chains = 3, thin = 3, control = list(adapt_delta = 0.99999999999999, max_treedepth =50),
+                                 init = initials1)
+
+traceplot(no_dist_seeds_brho_hi_hi, pars="lambda")
+pairs(no_dist_seeds_brho_hi_hi)
+
+### Save posterior distributions to file
+save(no_dist_seeds_brho_hi_hi, file = "no_dist_seeds_brho_hi_hi.rdata")
+
+## Look at resulting estimated parameter distributions
+stan_dens(no_dist_seeds_brho_hi_hi, pars = c("lambda", "alpha_pler", "alpha_brho", "alpha_lapl", "alpha_femi"))
+
+## Extract all parameter estimates
+brho_hi_hi <- rstan::extract(no_dist_seeds_brho_hi_hi)
+acf(brho_hi_hi$lambda)
+
+######################################################
+
+# high intermediate initials
+initials <- list(lambda=53.26, alpha_pler=0.161, alpha_brho=0.831, alpha_lapl=0.186,
+                 alpha_femi=0.368, epsilon=rep(1,P), sigma = 175.36)
+initials1<- list(initials, initials, initials)
+
+## Fit model
+### Specify the stan model, the data to send it by name, iterations, chains (based on number of cores), 
+### thinning constant (2 or 3 is usually fine), 
+# NOTE: NUMBER OF DIVERGENT TRANSITIONS IS 4; ALL RHAT VALUES ARE 1.00
+no_dist_seeds_brho_hi_int <- stan(file = "BRHO_Constrained_rplot_four_species_BH_model.stan", 
+                                 data = c("N", "Fecundity", "intra", "pler", "brho", "lapl", "femi", "P", "Plot","bg"),
+                                 iter = 12000, chains = 3, thin = 3, control = list(adapt_delta = 0.99999999999999, max_treedepth =50),
+                                 init = initials1)
+
+traceplot(no_dist_seeds_brho_hi_int, pars="lambda")
+pairs(no_dist_seeds_brho_hi_int)
+
+### Save posterior distributions to file
+save(no_dist_seeds_brho_hi_int, file = "no_dist_seeds_brho_hi_int.rdata")
+
+## Look at resulting estimated parameter distributions
+stan_dens(no_dist_seeds_brho_hi_int, pars = c("lambda", "alpha_pler", "alpha_brho", "alpha_lapl", "alpha_femi"))
+
+## Extract all parameter estimates
+brho_hi_int <- rstan::extract(no_dist_seeds_brho_hi_int)
+acf(brho_hi_int$lambda)
+
+######################################################
+
+# high low initials
+initials <- list(lambda=12.5, alpha_pler=0.1, alpha_brho=0.5, alpha_lapl=0.05,
+                 alpha_femi=0.4, epsilon=rep(1,P), sigma = 205)
+initials1<- list(initials, initials, initials)
+
+## Fit model
+### Specify the stan model, the data to send it by name, iterations, chains (based on number of cores), 
+### thinning constant (2 or 3 is usually fine), 
+# NOTE: NUMBER OF DIVERGENT TRANSITIONS IS 4; ALL RHAT VALUES ARE 1.00
+no_dist_seeds_brho_hi_lo <- stan(file = "BRHO_Constrained_rplot_four_species_BH_model.stan", 
+                                 data = c("N", "Fecundity", "intra", "pler", "brho", "lapl", "femi", "P", "Plot","bg"),
+                                 iter = 12000, chains = 3, thin = 3, control = list(adapt_delta = 0.9999999999999999, max_treedepth =50),
+                                 init = initials1)
+
+traceplot(no_dist_seeds_brho_hi_lo, pars="lambda")
+pairs(no_dist_seeds_brho_hi_lo)
+
+### Save posterior distributions to file
+save(no_dist_seeds_brho_hi_lo, file = "no_dist_seeds_brho_hi_lo.rdata")
+
+## Look at resulting estimated parameter distributions
+stan_dens(no_dist_seeds_brho_hi_lo, pars = c("lambda", "alpha_pler", "alpha_brho", "alpha_lapl", "alpha_femi"))
+
+## Extract all parameter estimates
+brho_hi_lo <- rstan::extract(no_dist_seeds_brho_hi_lo)
+acf(brho_hi_lo$lambda)
+
+######################################################
+
+# low high initials
+initials <- list(lambda=412, alpha_pler=0.5, alpha_brho=.05, alpha_lapl=0.4,
+                 alpha_femi=0.4, epsilon=rep(1,P), sigma = 569)
+initials1<- list(initials, initials, initials)
+
+#no_dist_seeds_brho_lo_hi <- stan(file = "BRHO_Rplot_four_species_BH_model.stan", 
+#                                 data = c("N", "Fecundity", "intra", "pler", "brho", "lapl", "femi", "P", "Plot","bg"),
+#                                 iter = 12000, chains = 3, thin = 3, control = list(adapt_delta = 0.99, max_treedepth =50),
+#                                 init = initials1)
+
+no_dist_seeds_brho_lo_hi <- stan(file = "lohi_constrained_BRHO_Rplot_four_species_BH_model.stan", 
+                                 data = c("N", "Fecundity", "intra", "pler", "brho", "lapl", "femi", "P", "Plot","bg"),
+                                 iter = 12000, chains = 3, thin = 3, control = list(adapt_delta = 0.9999, max_treedepth =50),
+                                 init = initials1)
+
+traceplot(no_dist_seeds_brho_lo_hi, pars="lambda")
+pairs(no_dist_seeds_brho_lo_hi)
+
+### Save posterior distributions to file
+save(no_dist_seeds_brho_lo_hi, file = "no_dist_seeds_brho_lo_hi.rdata")
+
+## Look at resulting estimated parameter distributions
+stan_dens(no_dist_seeds_brho_lo_hi, pars = c("lambda", "alpha_pler", "alpha_brho", "alpha_lapl", "alpha_femi"))
+
+pairs(no_dist_seeds_brho_lo_hi, pars = c("lambda", "alpha_pler", "alpha_brho", "alpha_lapl", "alpha_femi"))
+
+## Extract all parameter estimates
+brho_lo_hi <- rstan::extract(no_dist_seeds_brho_lo_hi)
+cor(brho_lo_hi$lambda, brho_lo_hi$alpha_pler) #-.03
+cor(brho_lo_hi$lambda, brho_lo_hi$alpha_lapl) #.003
+cor(brho_lo_hi$lambda, brho_lo_hi$alpha_femi) #.002
+cor(brho_lo_hi$lambda, brho_lo_hi$alpha_brho) #-0.012
+
+acf(brho_lo_hi$lambda)
+
+######################################################
+
+# low intermediate initials
+initials <- list(lambda=25, alpha_pler=0.08, alpha_brho=0.57, alpha_lapl=0.04,
+                 alpha_femi=0.07, epsilon=rep(1,P), sigma = 18)
+initials1<- list(initials, initials, initials)
+
+no_dist_seeds_brho2_lo_int <- stan(file = "BRHO_Constrained_rplot_four_species_BH_model.stan", 
+                                 data = c("N", "Fecundity", "intra", "pler", "brho", "lapl", "femi", "P", "Plot","bg"),
+                                 iter = 10000, chains = 3, thin = 3, control = list(adapt_delta = 0.9999999999, max_treedepth =50),
+                                 init = initials1)
+
+traceplot(no_dist_seeds_brho_lo_int, pars="lambda")
+pairs(no_dist_seeds_brho_lo_int)
+
+### Save posterior distributions to file
+save(no_dist_seeds_brho_lo_int, file = "no_dist_seeds_brho_lo_int.rdata")
+
+## Look at resulting estimated parameter distributions
+stan_dens(no_dist_seeds_brho_lo_int, pars = c("lambda", "alpha_pler", "alpha_brho", "alpha_lapl", "alpha_femi"))
+
+## Extract all parameter estimates
+brho_lo_int <- rstan::extract(no_dist_seeds_brho_lo_int)
+acf(brho_lo_int$lambda)
+
+######################################################
+
+# low low initials
+initials <- list(lambda=0.27, alpha_pler=0.01, alpha_brho=0.003, alpha_lapl=-0.001,
+                 alpha_femi=0.1, epsilon=rep(1,P), sigma = 7.9)
+initials1<- list(initials, initials, initials)
+
+no_dist_seeds_brho2_lo_lo <- stan(file = "BRHO_Constrained_rplot_four_species_BH_model2.stan", 
+                                  data = c("N", "Fecundity", "intra", "pler", "brho", "lapl", "femi", "P", "Plot","bg"),
+                                  iter = 12000, chains = 3, thin = 5, control = list(adapt_delta = 0.9999999999999999, max_treedepth =50),
+                                  init = initials1)
+
+traceplot(no_dist_seeds_brho_lo_lo, pars="lambda")
+pairs(no_dist_seeds_brho_lo_lo)
+
+### Save posterior distributions to file
+save(no_dist_seeds_brho_lo_lo, file = "no_dist_seeds_brho_lo_lo.rdata")
+
+## Look at resulting estimated parameter distributions
+stan_dens(no_dist_seeds_brho_lo_lo, pars = c("lambda", "alpha_pler", "alpha_brho", "alpha_lapl", "alpha_femi"))
+
+## Extract all parameter estimates
+brho_lo_lo <- rstan::extract(no_dist_seeds_brho_lo_lo)
+acf(brho_lo_lo$lambda)
